bfc952d94cd4a429ca762431ca98281042ea1712
darynye <daryn100@mail.ru>
2024-02-09 19:18:13 +0600

    first change

---
+++ app/src/main/java/com/example/sis2107_9feb_intents/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sis2107_9feb_intents
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
